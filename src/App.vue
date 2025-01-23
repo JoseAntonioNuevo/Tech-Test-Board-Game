d187f4ce4af85287c2f503b7875459b25d34e6e6
José Antonio Nuevo Claverías <joseantonionuevo@gmail.com>
2025-01-23 21:58:18 +0100

    refactor: restructure app to implement game size selection and initial page component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <InitialPage v-if="!gameInProgress" @start-game="startGame" />
+    <div v-else>
+      <p>Game is in progress...</p>
+      <button @click="exitGame">Exit</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import InitialPage from "./InitialPage/Components/InitialPage.vue";
+
+export default defineComponent({
+  components: {
+    InitialPage,
+  },
+  setup() {
+    const gameInProgress = ref<boolean>(false);
+    const gameSize = ref<number>(1);
+
+    const startGame = (newGameSize: number): void => {
+      gameSize.value = newGameSize;
+      gameInProgress.value = true;
+    };
+
+    const exitGame = (): void => {
+      gameSize.value = 1;
+      gameInProgress.value = false;
+    };
+
+    return {
+      gameInProgress,
+      startGame,
+      exitGame,
+    };
+  },
+});
+</script>
---
+++ src/InitialPage/Components/InitialPage.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="hello-world">
+    <h1>Welcome to The Tech Test Board Game</h1>
+    <h2>Choose The Game Size</h2>
+    <div class="game-size-form">
+      <input
+        @input="setGameSize($event)"
+        type="number"
+        name="game-size"
+        id="game-size"
+        min="1"
+        max="25"
+        v-model.number="gameSize"
+      />
+      <button @click="startGame">Start</button>
+    </div>
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import "../Assets/Css/InitialPage.css";
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "InitialComponent",
+  emits: ["start-game"],
+  setup(_, { emit }) {
+    const gameSize = ref<number>(1);
+    const error = ref<string>("");
+
+    const setGameSize = (e: Event): void => {
+      const target = e.target as HTMLInputElement;
+      const value = target.value ? parseInt(target.value) : 1;
+      CheckGameSize(value);
+    };
+
+    const CheckGameSize = (value: number): boolean => {
+      const maxSize = 25;
+      const minSize = 1;
+
+      if (value > maxSize) {
+        gameSize.value = maxSize;
+        error.value = `The maximum game size is ${maxSize}.`;
+        return false;
+      }
+      if (value < minSize) {
+        gameSize.value = minSize;
+        error.value = `The minimum game size is ${minSize}.`;
+        return false;
+      }
+
+      gameSize.value = value;
+      error.value = "";
+      return true;
+    };
+
+    const startGame = (): void => {
+      if (CheckGameSize(gameSize.value)) {
+        emit("start-game", gameSize.value);
+        return;
+      }
+      error.value = "Please enter a valid game size.";
+    };
+
+    return {
+      gameSize,
+      error,
+      setGameSize,
+      startGame,
+    };
+  },
+});
+</script>
